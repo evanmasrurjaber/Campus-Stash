b82ff5fba3d7e57872b43954ab838957216f01a2
Mahmudul Hasan Tamal <118817708+mhtamal246@users.noreply.github.com>
2026-04-03 02:29:12 +0600

    errorfix: signup backend

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { login } = require('../controllers/authController');
+const { login, signup } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
